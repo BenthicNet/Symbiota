ac236595ea0416375ea0904fdbcf79fa3273d0dc
Christopher Tyrrell <43977100+cdtyrrell@users.noreply.github.com>
2026-04-07 10:14:38 -0500

    logo links

--- includes/footer.php
+++ includes/footer.php
@@ -6,16 +6,17 @@
 		<a href="https://freshwater.wisconsin.edu/" target="_blank" title="<?= $LANG['F_FCW'] ?>" aria-label="<?= $LANG['F_FCW'] ?>">
 			<img src="<?= $CLIENT_ROOT; ?>/images/layout/logo_fcw.png"  alt="<?= $LANG['F_FCW_LOGO'] ?>" />
 		</a>
-				<a href="https://freshwater.wisconsin.edu/" target="_blank" title="<?= $LANG['F_FCW'] ?>" aria-label="<?= $LANG['F_FCW'] ?>">
+		<a href="https://www.uwp.edu/learn/beyondtheclassroom/research/naturalhistorycollections.cfm" target="_blank" title="<?= $LANG['F_FCW'] ?>" aria-label="<?= $LANG['F_FCW'] ?>">
 			<img src="<?= $CLIENT_ROOT; ?>/images/layout/logo_uwp.png"  alt="<?= $LANG['F_FCW_LOGO'] ?>" />
 		</a>
-				<a href="https://freshwater.wisconsin.edu/" target="_blank" title="<?= $LANG['F_FCW'] ?>" aria-label="<?= $LANG['F_FCW'] ?>">
-			<img src="<?= $CLIENT_ROOT; ?>/images/layout/logo_uwsp.png"  alt="<?= $LANG['F_FCW_LOGO'] ?>" />
-		</a>
-				<a href="https://freshwater.wisconsin.edu/" target="_blank" title="<?= $LANG['F_FCW'] ?>" aria-label="<?= $LANG['F_FCW'] ?>">
+		<a href="https://wirc.wisc.edu/" target="_blank" title="<?= $LANG['F_FCW'] ?>" aria-label="<?= $LANG['F_FCW'] ?>">
 			<img src="<?= $CLIENT_ROOT; ?>/images/layout/logo_wisc.png"  alt="<?= $LANG['F_FCW_LOGO'] ?>" />
 		</a>
-				<a href="https://freshwater.wisconsin.edu/" target="_blank" title="<?= $LANG['F_FCW'] ?>" aria-label="<?= $LANG['F_FCW'] ?>">
+		<img> <!-- empty tag to align flexbox grid -->
+		<a href="https://www3.uwsp.edu/cnr-ap/biomonitoring/Pages/default.aspx" target="_blank" title="<?= $LANG['F_FCW'] ?>" aria-label="<?= $LANG['F_FCW'] ?>">
+			<img src="<?= $CLIENT_ROOT; ?>/images/layout/logo_uwsp.png"  alt="<?= $LANG['F_FCW_LOGO'] ?>" />
+		</a>
+		<a href="https://www.mpm.edu/" target="_blank" title="<?= $LANG['F_FCW'] ?>" aria-label="<?= $LANG['F_FCW'] ?>">
 			<img src="<?= $CLIENT_ROOT; ?>/images/layout/logo_mpm.png"  alt="<?= $LANG['F_FCW_LOGO'] ?>" />
 		</a>
 	</div>
