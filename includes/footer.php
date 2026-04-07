8bd3e01c09df24aadc6648e75360c96ee22321ce
Christopher Tyrrell <43977100+cdtyrrell@users.noreply.github.com>
2026-04-07 09:25:02 -0500

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -6,6 +6,18 @@
 		<a href="https://freshwater.wisconsin.edu/" target="_blank" title="<?= $LANG['F_FCW'] ?>" aria-label="<?= $LANG['F_FCW'] ?>">
 			<img src="<?= $CLIENT_ROOT; ?>/images/layout/FCW.png"  alt="<?= $LANG['F_FCW_LOGO'] ?>" />
 		</a>
+				<a href="https://freshwater.wisconsin.edu/" target="_blank" title="<?= $LANG['F_FCW'] ?>" aria-label="<?= $LANG['F_FCW'] ?>">
+			<img src="<?= $CLIENT_ROOT; ?>/images/layout/FCW.png"  alt="<?= $LANG['F_FCW_LOGO'] ?>" />
+		</a>
+				<a href="https://freshwater.wisconsin.edu/" target="_blank" title="<?= $LANG['F_FCW'] ?>" aria-label="<?= $LANG['F_FCW'] ?>">
+			<img src="<?= $CLIENT_ROOT; ?>/images/layout/FCW.png"  alt="<?= $LANG['F_FCW_LOGO'] ?>" />
+		</a>
+				<a href="https://freshwater.wisconsin.edu/" target="_blank" title="<?= $LANG['F_FCW'] ?>" aria-label="<?= $LANG['F_FCW'] ?>">
+			<img src="<?= $CLIENT_ROOT; ?>/images/layout/FCW.png"  alt="<?= $LANG['F_FCW_LOGO'] ?>" />
+		</a>
+				<a href="https://freshwater.wisconsin.edu/" target="_blank" title="<?= $LANG['F_FCW'] ?>" aria-label="<?= $LANG['F_FCW'] ?>">
+			<img src="<?= $CLIENT_ROOT; ?>/images/layout/FCW.png"  alt="<?= $LANG['F_FCW_LOGO'] ?>" />
+		</a>
 	</div>
 	<p>
 		<?= $LANG['F_POWERED_BY'] ?> <a href="https://symbiota.org/" target="_blank">Symbiota</a> v3.4.1.
