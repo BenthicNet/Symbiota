50bcd06821bff010f229cafb67827a3a4e204b9f
Christopher Tyrrell <43977100+cdtyrrell@users.noreply.github.com>
2026-05-15 08:10:33 -0500

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,26 +1,10 @@
 <footer>
 	<div class="logo-gallery">
-		<?php
-		//include($SERVER_ROOT . '/accessibility/module.php');
-		?>
 		<a href="https://freshwater.wisconsin.edu/" target="_blank" title="<?= $LANG['F_FCW'] ?>" aria-label="<?= $LANG['F_FCW'] ?>">
 			<img src="<?= $CLIENT_ROOT; ?>/images/layout/logo_fcw.png"  alt="<?= $LANG['F_FCW_LOGO'] ?>" />
 		</a>
-		<a href="https://www.uwp.edu/learn/beyondtheclassroom/research/naturalhistorycollections.cfm" target="_blank" title="<?= $LANG['F_FCW'] ?>" aria-label="<?= $LANG['F_FCW'] ?>">
-			<img src="<?= $CLIENT_ROOT; ?>/images/layout/logo_uwp.png"  alt="<?= $LANG['F_FCW_LOGO'] ?>" />
-		</a>
-		<a href="https://wirc.wisc.edu/" target="_blank" title="<?= $LANG['F_FCW'] ?>" aria-label="<?= $LANG['F_FCW'] ?>">
-			<img src="<?= $CLIENT_ROOT; ?>/images/layout/logo_wisc.png"  alt="<?= $LANG['F_FCW_LOGO'] ?>" />
-		</a>
-		<img> <!-- empty tag to align flexbox grid -->
-		<a href="https://www3.uwsp.edu/cnr-ap/biomonitoring/Pages/default.aspx" target="_blank" title="<?= $LANG['F_FCW'] ?>" aria-label="<?= $LANG['F_FCW'] ?>">
-			<img src="<?= $CLIENT_ROOT; ?>/images/layout/logo_uwsp.png"  alt="<?= $LANG['F_FCW_LOGO'] ?>" />
-		</a>
-		<a href="https://www.mpm.edu/" target="_blank" title="<?= $LANG['F_FCW'] ?>" aria-label="<?= $LANG['F_FCW'] ?>">
-			<img src="<?= $CLIENT_ROOT; ?>/images/layout/logo_mpm.png"  alt="<?= $LANG['F_FCW_LOGO'] ?>" />
-		</a>
 	</div>
 	<p>
-		<?= $LANG['F_POWERED_BY'] ?> <a href="https://symbiota.org/" target="_blank">Symbiota</a> v3.4.1.
+		<?= $LANG['F_POWERED_BY'] ?> <a href="https://symbiota.org/" target="_blank">Symbiota</a> <?= $CODE_VERSION ?>.
 	</p>
 </footer>
